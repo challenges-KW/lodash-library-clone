84ad9a0f01fd92fea997e6944626c7580948f848
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-18 10:54:36 -0500

    added first, last, foreach

--- library.js
+++ library.js
@@ -1,12 +1,15 @@
 module.exports = {
     identity : param => param,
-    first: (arr, indexOfArr) => {
+    first: function (arr, indexOfArr) {
 //if there is no other args, return the first element of the array
-        if (indexOfArr > 0) {
+        if (arguments.length === 1) {
             return arr[0]
-        } else if (indexOfArr === 0) {
+al        } else if (indexOfArr === 0) {
             return []
-        } else {
+        } else if (indexOfArr > arr.length) {
+            return arr
+        }
+        else {
        //return a new array
        //this array will have only elements up to the index 
        //Steps:
@@ -18,9 +21,40 @@ module.exports = {
             newArr.push(arr[i])
         }
        //return that new array
-       console.log('this is newArr after push:', newArr)
+       console.log('this is newArr: ', newArr)
        return newArr
     }
 
-}
+},
+last: function (arr, indexOfArr) {
+    //if there is no other args, return the last element of the array
+            if (arguments.length === 1) {
+
+                return arr[arr.length - 1]
+    al        } else if (indexOfArr === 0) {
+                return []
+            } else if (indexOfArr > arr.length - 1 ) {
+                return arr
+            }
+            else {
+           //return a new array
+           //this array will have only elements up to the index 
+           //Steps:
+           //create a new arr
+           let newArr = []
+    //loop through the old array up to the point of the indexOfArr
+            for (i = indexOfArr; i < arr.length; i++) {
+                //push into the new array elements from the old array
+                newArr.push(arr[i])
+            }
+           //return that new array
+           return newArr
+        }
+    
+    },
+    foreach: function (arr, funct) {
+       for (var i = 0; i < arr.length; i++){
+        funct(arr[i])
+    }
+ }
 }
--- library.spec.js
+++ library.spec.js
@@ -18,17 +18,79 @@ describe('Library', ()=> {
             uniqueArray = [0, 1, 2]
           })            
     
-        test('Should return first element of array', () => {
+        test('Should return first element of array if the function is passed a single argument', () => {
             expect(_.first(uniqueArray)).toBe(0)
         })
-        test('Should accept an index argument', () => {
-            expect(_.first(uniqueArray, 1)).to.eql([0, 1])
+        test('Should accept another argument as an index, and return a slice of the array from 0 upto and including the index', () => {
+            expect(_.first(uniqueArray, 1)).toStrictEqual([0, 1])
         })
 
         test('Should return an empty array if the index passed is zero', () => {
-            expect(_.first(uniqueArray, 0)).to.eql([])
+            expect(_.first(uniqueArray, 0)).toStrictEqual([])
         })
 
+        test ('If the index argument is greater than the length of the array, return full array', () => {
+            expect(_.first(uniqueArray, 5)).toStrictEqual([0, 1, 2])
+        })
+    
     })
 
+    describe('last', () => {
+        let uniqueArray
+
+        beforeEach(() => {
+            uniqueArray = [0, 1, 2]
+          })            
+    
+        test('Should return last element of array if the function is passed a single argument', () => {
+            expect(_.last(uniqueArray)).toBe(2)
+        })
+        test('Should accept another argument as an index, and return a slice of the array from the array.length minus the index upto and including the final element in the array', () => {
+            expect(_.last(uniqueArray, 1)).toStrictEqual([1, 2])
+        })
+
+        test('Should return an empty array if the index passed is zero', () => {
+            expect(_.last(uniqueArray, 0)).toStrictEqual([])
+        })
+
+        test ('If the index argument is greater than the length of the array, return full array', () => {
+            expect(_.last(uniqueArray, 5)).toStrictEqual([0, 1, 2])
+        })
+    
+    })
+
+    describe('Foreach', () => {
+        let uniqueArray
+
+        beforeEach(() => {
+            uniqueArray = [0, 1, 2, 5, 4]
+          }) 
+       test('should not return anything', () => {
+        let result = _.foreach(uniqueArray, _.identity)
+        expect(uniqueArray).toStrictEqual([ 0, 1, 2, 5, 4 ])
+
+       })
+
+       test('Should invoke a callback on each element in an array', () => {
+          let resultArray = []
+
+           let testFunction = x => x * x
+
+           _.foreach(uniqueArray, (element) => {
+               resultArray.push(testFunction(element))
+           })
+           expect(resultArray).toStrictEqual([0, 1, 4, 25, 16]) 
+           expect(uniqueArray).toStrictEqual([0, 1, 2, 5, 4]) 
+       })
+
+       test('Should provide access to each index of the array', () => {
+        let testFunction = x => x * x
+        let resultArray = []
+
+           _.foreach(uniqueArray, (element) => {
+               resultArray.push(testFunction(element) + ':'+ index)
+           })
+           expect(resultArray).toStrictEqual(['0:0', '1:1', '4:2', '25:3', '16:4'])    
+       } )
+    })
 })
