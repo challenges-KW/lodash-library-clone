435e77d712f747aa56dbf5f8005638a14fff1221
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-18 19:33:31 -0500

    foreach in library.js written for 'should provide access to each index

--- library.js
+++ library.js
@@ -54,7 +54,7 @@ last: function (arr, indexOfArr) {
     },
     foreach: function (arr, funct) {
        for (var i = 0; i < arr.length; i++){
-        funct(arr[i])
+        funct(arr[i], i)
     }
  }
 }
