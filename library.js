81b2e57bc9282c3d4ea4df1c5e45f71172b59da4
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-21 18:26:49 -0500

    indexOf written with loop before refactoring to use foreach inside indexOf and tests refactored to include beforeeach in root describe to encompass nested describes

--- library.js
+++ library.js
@@ -32,9 +32,32 @@ last: function (arr, indexOfArr) {
         }
     
     },
-    foreach: function (arr, funct) {
-       for (let i = 0; i < arr.length; i++){
-        funct(arr[i], i, arr);
+    foreach: function (arrOrObj, funct) {
+        if (Array.isArray(arrOrObj)) {
+            for (let i = 0; i < arrOrObj.length; i++){
+                funct(arrOrObj[i], i, arrOrObj);
+            }
+        } else {
+            for (let key in arrOrObj) {
+                if (arrOrObj.hasOwnProperty(key)) {
+                funct(arrOrObj[key], key, arrOrObj);
+                }
+            }
+        }
+    },
+    indexOf: function(arr, element) {
+//refactor using foreach method
+
+        // loop through  the array
+        for (let i = 0; i < arr.length; i++) {
+
+        // compare the element that was passed in against the other elements in the array
+        if (arr[i] === element) {
+        //if you find it, return its index
+            return i
+        }
+        //Todo: what happens if it is not found?
+        }
+        return -1
     }
- }
 }
--- library.spec.js
+++ library.spec.js
@@ -1,9 +1,23 @@
 const _ = require('./library.js')
 
 describe('Library', ()=> {
+    let uniqueArray
+    const uniqueObject = {}
+    let resultArray
+    let testFunction
+    let testArr
+    let resultObj
+
+    beforeEach(() => {
+        uniqueArray = [0, 5, 1, 2, 5, 4, 5]
+        resultArray = []
+        testArr = []
+        testFunction = x => x * x
+        resultObj = {a : 'a', b: 'b'}
+    })
+
     describe('identity', () => {
         test('Should return whatever is passed to it', () => {
-            const uniqueObject = {}
             expect(_.identity(1)).toBe(1)
             expect(_.identity('string')).toBe('string')
             expect(_.identity(false)).toBeFalsy
@@ -12,17 +26,11 @@ describe('Library', ()=> {
     })
 
     describe('first', () => {
-        let uniqueArray
-
-        beforeEach(() => {
-            uniqueArray = [0, 1, 2]
-          })            
-    
         test('Should return first element of array if the function is passed a single argument', () => {
             expect(_.first(uniqueArray)).toBe(0)
         })
         test('Should accept another argument as an index, and return a slice of the array from 0 upto and including the index', () => {
-            expect(_.first(uniqueArray, 1)).toStrictEqual([0, 1])
+            expect(_.first(uniqueArray, 1)).toStrictEqual([0, 5])
         })
 
         test('Should return an empty array if the index passed is zero', () => {
@@ -30,23 +38,17 @@ describe('Library', ()=> {
         })
 
         test ('If the index argument is greater than the length of the array, return full array', () => {
-            expect(_.first(uniqueArray, 5)).toStrictEqual([0, 1, 2])
+            expect(_.first(uniqueArray, 8)).toStrictEqual([0, 5, 1, 2, 5, 4, 5])
         })
     
     })
 
     describe('last', () => {
-        let uniqueArray
-
-        beforeEach(() => {
-            uniqueArray = [0, 1, 2]
-          })            
-    
         test('Should return last element of array if the function is passed a single argument', () => {
-            expect(_.last(uniqueArray)).toBe(2)
+            expect(_.last(uniqueArray)).toBe(5)
         })
         test('Should accept another argument as an index, and return a slice of the array from the array.length minus the index upto and including the final element in the array', () => {
-            expect(_.last(uniqueArray, 1)).toStrictEqual([1, 2])
+            expect(_.last(uniqueArray, 5)).toStrictEqual([4, 5])
         })
 
         test('Should return an empty array if the index passed is zero', () => {
@@ -54,70 +56,69 @@ describe('Library', ()=> {
         })
 
         test ('If the index argument is greater than the length of the array, return full array', () => {
-            expect(_.last(uniqueArray, 5)).toStrictEqual([0, 1, 2])
+            expect(_.last(uniqueArray, 7)).toStrictEqual([0, 5, 1, 2, 5, 4, 5])
         })
     
     })
 
     describe('Foreach', () => {
-        let uniqueArray
-
-        beforeEach(() => {
-            uniqueArray = [0, 1, 2, 5, 4]
-          }) 
        test('should not return anything', () => {
         let result = _.foreach(uniqueArray, _.identity)
-        expect(uniqueArray).toStrictEqual([ 0, 1, 2, 5, 4 ])
+        expect(uniqueArray).toStrictEqual([ 0, 5, 1, 2, 5, 4, 5 ])
 
        })
 
        test('Should invoke a callback on each element in an array', () => {
-          let resultArray = []
-
-           let testFunction = x => x * x
 
            _.foreach(uniqueArray, (element) => {
                resultArray.push(testFunction(element))
            })
-           expect(resultArray).toStrictEqual([0, 1, 4, 25, 16]) 
-           expect(uniqueArray).toStrictEqual([0, 1, 2, 5, 4]) 
+           expect(resultArray).toStrictEqual([0, 25, 1, 4, 25, 16, 25]) 
+           expect(uniqueArray).toStrictEqual([0, 5, 1, 2, 5, 4, 5]) 
        })
 
        test('Should provide access to each index of the array', () => {
-        let testFunction = x => x * x
-        let resultArray = []
-
-
            _.foreach(uniqueArray, (element, index) => {
                resultArray.push(testFunction(element) + ':' + index)
            })
-           expect(resultArray).toStrictEqual(['0:0', '1:1', '4:2', '25:3', '16:4'])    
+           expect(resultArray).toStrictEqual(['0:0', '25:1', '1:2', '4:3', '25:4', '16:5', '25:6'])    
        } )
 
        test('Should provide access to the original array passed to it', () => {
-           let testArr = []
            _.foreach(uniqueArray, (element, index) => {
                testArr.push([element, index, uniqueArray])
            })
-           expect(testArr[0]).toStrictEqual([0, 0,[0, 1, 2, 5, 4]])
+           expect(testArr[0]).toStrictEqual([0, 0,[0, 5, 1, 2, 5, 4, 5]])
         })
 
         test('Should provide access on each value in an object', () => {
-            let resultObj = {a : 'a', b: 'b'}
-            let resultArray = []
              _.foreach(resultObj, (value) => {
-                 resultArray.push(testFunction(value + 'ye'))
+                 resultArray.push((value + 'ye'))
              })
              expect(resultArray).toStrictEqual(['aye', 'bye']) 
          })
 
          test('Should provide access to each key and the original object passed to it', () => {
-            let testArr = []
-            let resultObj = {a : 'a', b: 'b'}
             _.foreach(resultObj, (value, key) => {
-                testArr.push([value, key, testObj])
+                testArr.push([value, key, resultObj])
             })
-            expect(testArr[0]).toStrictEqual(['a', 'a', resultArray])
+            expect(testArr[0]).toStrictEqual(['a', 'a', resultObj])
          })
+    })
+
+    describe('indexOf', () => {        
+        test('Should show index number of given element', () => {
+
+           expect(_.indexOf(uniqueArray, 5)).toStrictEqual(1)
+        })
+        
+        test('Should show index number of the first element it encounters in the array', () => {
+
+            expect(_.indexOf(uniqueArray, 5)).toStrictEqual(1)
+         })
+        test('Should return -1 if element not found in array', () => {
+
+            expect(_.indexOf(uniqueArray, 7)).toStrictEqual(-1)
+        })
     })
 })
